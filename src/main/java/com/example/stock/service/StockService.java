00064a3aa0de9cee5b24c61b9c04006b562004e9
newbalancer <pnw123456@icloud.com>
2022-10-10 16:40:30 +0900

    feat: 제고 감소 테스트 케이스

---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,22 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    @Transactional
+    public void decrease(Long stockId , Long quantity){
+        Stock stock = stockRepository.findById(stockId).orElseThrow();
+        stock.decrease(quantity);
+    }
+}
