8aaae77f19eff4eded8ae468195e633f2239853e
newbalancer <pnw123456@icloud.com>
2022-10-10 18:38:20 +0900

    feat: namedlock example add

---
+++ src/test/java/com/example/stock/facade/NamedLockStockFacadeTest.java
@@ -0,0 +1,72 @@
+package com.example.stock.facade;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.LockRepository;
+import com.example.stock.repository.StockRepository;
+import com.example.stock.service.StockService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class NamedLockStockFacadeTest {
+
+    @Autowired
+    private NamedLockStockFacade facade;
+
+    @Autowired
+    private LockRepository lockRepository;
+
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+
+    @BeforeEach
+    void init() {
+        Stock stock = new Stock(null, 1L, 100L);
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    void decrease_100thread() throws InterruptedException {
+
+        int count = 100;
+
+        ExecutorService executorService =
+                Executors.newFixedThreadPool(32);
+
+        CountDownLatch latch = new CountDownLatch(count);
+
+        for (int i = 0; i < count; i++) {
+            executorService.execute(() -> {
+                try {
+                    facade.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(0L, stock.getQuantity());
+    }
+}
