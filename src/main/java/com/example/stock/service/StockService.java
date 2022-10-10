b9a0c8caa345b96a41b3d80fb026c5283dba727c
newbalancer <pnw123456@icloud.com>
2022-10-10 17:27:43 +0900

    feat: Synchronized method add

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,9 +4,6 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +11,11 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
-    @Transactional
+    //@Transactional
     public synchronized Stock decrease(Long stockId , Long quantity){
         Stock stock = stockRepository.findById(stockId).orElseThrow();
         stock.decrease(quantity);
+        stockRepository.saveAndFlush(stock);
         return stock;
     }
 }
